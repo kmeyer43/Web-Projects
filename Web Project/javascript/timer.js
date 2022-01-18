152f3d986ec986aa70b12daca46498c36c2d99f8
kmeyer43 <77902570+kmeyer43@users.noreply.github.com>
2022-01-18 16:57:20 -0600

    Added test case to timer.js file, updated styles

--- Web Project/javascript/timer.js
+++ Web Project/javascript/timer.js
@@ -1,1 +1,9 @@
-var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+// Create a count down timer that recieves input from the user and outputs the time in seconds
+// Test code:
+$(document).ready(function(){
+ 
+    $('#startButton').click(function(){
+     alert("button clicked");
+    });
+   });
+
