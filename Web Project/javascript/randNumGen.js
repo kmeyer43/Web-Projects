08b8869a428755935b1373156e7edd376bad351c
kmeyer43 <77902570+kmeyer43@users.noreply.github.com>
2022-01-21 09:30:31 -0600

    Added more buttons to random number generator page
    Added more buttons to random number generator page and added functionality to the random number generator page. Changed styling slightly as well.

---
+++ Web Project/javascript/randNumGen.js
@@ -0,0 +1,26 @@
+// Create a random number generator that generates a random number between 0 and 100 when the user clicks the button.
+// Test code:
+ $(document).ready(function(){
+
+    $('#randNumBtn10').click(function() {
+    
+    var randomNumber = Math.floor((Math.random() * 10) + 1);
+        $('#randomNum10').html(randomNumber);
+         // alert(randomNumber);
+     });
+
+    $('#randNumBtn100').click(function() {
+    
+    var randomNumber = Math.floor((Math.random() * 100) + 1);
+           $('#randomNum100').html(randomNumber);
+            // alert(randomNumber);
+        });
+
+        $('#randNumBtn1000').click(function() {
+    
+            var randomNumber = Math.floor((Math.random() * 1000) + 1);
+               $('#randomNum1000').html(randomNumber);
+                // alert(randomNumber);
+            });
+
+    });
